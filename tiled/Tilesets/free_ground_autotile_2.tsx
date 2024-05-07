5d7a2afb0b73a44a131edc5946599ca79d9dcf44
Terrastronghold <sonicx702@gmail.com>
2024-05-07 01:32:04 -0600

    did stuff

---
+++ tiled/Tilesets/free_ground_autotile_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="free_ground_autotile_2" tilewidth="32" tileheight="32" tilecount="4" columns="2">
+ <image source="../../assets/Tilesets/free_ground_autotile_2.png" width="64" height="64"/>
+</tileset>
---
+++ tiled/Tilesets/grass_town.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="grass_town" tilewidth="16" tileheight="16" tilecount="120" columns="8">
+ <image source="../../assets/Tilesets/grass_town.png" width="128" height="240"/>
+</tileset>
