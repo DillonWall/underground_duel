083db103d3dbd9e7d5d288fb602e1894fb48cf18
Dillon Wall <dillonmwall@msn.com>
2024-05-07 17:44:34 +0800

    Asset reorganized and master png

---
+++ tiled/Tilemaps/Master.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Master" tilewidth="16" tileheight="16" tilecount="4248" columns="72">
+ <image source="../../assets/Tilesets/Master.png" width="1152" height="944"/>
+ <tile id="223">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="0" y="1" width="16" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="366">
+  <objectgroup draworder="index" id="2">
+   <object id="6" x="0" y="2" width="16" height="13"/>
+  </objectgroup>
+ </tile>
+ <tile id="437">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4" y="3" width="12" height="13"/>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ tiled/Tilesets/Master.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Master" tilewidth="16" tileheight="16" tilecount="4248" columns="72">
+ <image source="../../assets/Tilesets/Master.png" width="1152" height="944"/>
+</tileset>
